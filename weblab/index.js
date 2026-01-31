86cf3c29354a417ca4c1ecf550af0e1a602ce1fd
kimdagus06 <149805406+kimdagus06@users.noreply.github.com>
2026-01-31 16:26:08 +0100

    Update index.js

--- weblab/index.js
+++ weblab/index.js
@@ -1,1 +1,15 @@
-console("Welcome to the Advanced Web Development Course!");
+// Strings
+let d = "ddddddddd";
+// Numbers
+let e = 1 + 3;
+let f = 2.5 + 3.8;
+// Booleans
+let g = true;
+let h = false;
+/// Empty Values
+// null and undefined
+// mostly interchangeable in JS
+let p = null;
+console.log(p);
+p = undefined;
+console.log(p);
