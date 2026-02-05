dea32ca42d9e75e6cd9f09fc99208c821de64271
kimdagus06 <149805406+kimdagus06@users.noreply.github.com>
2026-02-05 14:39:48 +0100

    update assignment 1 - part 1

---
+++ weblab/lab1-part1.js
@@ -0,0 +1,22 @@
+/* 1. Variables and Data Types
+Task: Create variables of different data types as well as array, set and objectand log them to the console.
+*/
+
+alert("JS 파일이 연결되었습니다!"); // 이 창이 뜨는지 확인해보세요.
+
+const name = "Dasha";
+let age = 32;
+let isStudent = true;
+
+let hobbies = ["playing", "coding", "traveling"];
+let animals = new Set(["cat", "dog", "tiger"]);
+let user = {
+  name: "Dasha",
+  age: 32,
+  isStudent: true,
+};
+
+console.log(name, age, isStudent);
+console.log(hobbies);
+console.log(animals);
+console.log(user);
